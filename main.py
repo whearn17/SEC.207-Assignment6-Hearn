a344265ad98428aa2494b701bbf72a9c6c8ed504
whearn17 <74379583+whearn17@users.noreply.github.com>
2021-11-01 14:13:52 -0400

    first commit

--- main.py
+++ main.py
@@ -2,26 +2,18 @@
 # Python Programming
 
 
-def pig_latin(text):
+def split(text):
     text = text.split()
-    pig = ""
-    end = False
-    tmp = ""
     for word in text:
-        tmp = ""
-        if word[0] in "aeiouyAEIOUY":
-            pig += word + "yay "
-        elif word[0] not in "aeiouyAEIOUY":
-            for character in word:
-                if character in "aeiouyAEIOUY" and not end:
-                    end = True
-                elif character not in "aeiouyAEIOUY" and not end:
-                    tmp += character
-            end = False
-            pig += word[len(tmp):] + word[0:len(tmp)] + "ay "
-    return pig
+        print(word)
+
+
+def split2(text):
+    print(text.replace(" ", "\n"))
 
 
 if __name__ == '__main__':
-    print(pig_latin(input("Enter a message ")))
+    split(input("Enter a word "))
+    split2(input("Enter a word "))
+
 
