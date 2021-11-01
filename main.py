8ecfe82cf256f3f7dfe1706ff95ad544b0b51ee7
whearn17 <74379583+whearn17@users.noreply.github.com>
2021-10-31 20:47:54 -0400

    first commit

--- main.py
+++ main.py
@@ -2,19 +2,26 @@
 # Python Programming
 
 
-def secret(text):
-    coded_text = ""
-    for character in text:
-        coded_text += chr(ord(character) + 5)
-    return coded_text
-
-
-def multiply(text):
-    return text * 5
+def pig_latin(text):
+    text = text.split()
+    pig = ""
+    end = False
+    tmp = ""
+    for word in text:
+        tmp = ""
+        if word[0] in "aeiouyAEIOUY":
+            pig += word + "yay "
+        elif word[0] not in "aeiouyAEIOUY":
+            for character in word:
+                if character in "aeiouyAEIOUY" and not end:
+                    end = True
+                elif character not in "aeiouyAEIOUY" and not end:
+                    tmp += character
+            end = False
+            pig += word[len(tmp):] + word[0:len(tmp)] + "ay "
+    return pig
 
 
 if __name__ == '__main__':
-    print(secret(input("Enter a message")))
-    print(multiply(input("Enter a message")))
-
+    print(pig_latin(input("Enter a message")))
 
